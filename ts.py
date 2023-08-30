3bb5f52df5d250c4d4135229d712e551cc3a9321
HIUHIU <88829490+hieungo0503@users.noreply.github.com>
2023-08-30 17:21:13 +0700

    new

--- ts.py
+++ ts.py
@@ -4,20 +4,29 @@ input_file = "input.txt"
 data_Cell_S = []
 data_Cell_N = []
 data_cell_Rank =[]
+data_Cell_Start = []
+data_Cell_Fail = []
 
 with open(input_file, "r") as file:
     current_entry = {}
-    check_1=check_2=check_3 = False
+    check_1=check_2=check_3=check_4=check_5 = False
     for line in file:
         line = line.strip()
         if "***" in line:
-            check_1=check_2=check_3 = False
+            check_1=check_2=check_3=check_4=check_5 = False
         if "Serving Cell Measurements Response" in line:
             check_1 = True
         elif "Neighbor Cell Measurements" in line:  
             check_2 = True
+        elif "Reselection Started Event" in line:
+            
+            check_4 = True
+        elif "Reselection Failure Event" in line:  
+            check_5 = True
+
         elif "Cell Reselection" in line:      #for RANK  
             check_3 = True
+        
         
         if check_1:
             if line.startswith("Time :"):
@@ -76,13 +85,32 @@ with open(input_file, "r") as file:
             #     elif "Neighbor Cell Measurements" in line:  
             #         check_2 = True
             #     check_3 = False
+        if check_4:
+            print("testttttt")
+            if "Time :" in line:
+                current_entry["Time"] = line.split(" : ")[1].strip()
+            elif "PCI :" in line:
+                current_entry["PCI"] = line.split(":")[1].strip()
+                data_Cell_Start.append(current_entry)
+                current_entry = {}
+        if check_5:
+            if "Time :" in line:
+                current_entry["Time"] = line.split(" : ")[1].strip()
+            elif "PCI :" in line:
+                current_entry["PCI"] = line.split(":")[1].strip()
+            elif "CellReselFailureCause :" in line:
+                current_entry["CellReselFailureCause"] = line.split(":")[1].strip()
+                data_Cell_Fail.append(current_entry)
+                current_entry = {}
+            
 
-
+# Reselection Started Event
+# Reselection Failure Event CellReselFailureCause
             
         
 # Print collected data_Cell_S (for verification)
-for entry in data_Cell_S:
-    print(entry)
+# for entry in data_Cell_S:
+#     print(entry)
 
 out_file = "out_S_cell.xlsx"
 
@@ -105,8 +133,8 @@ for entry in data_Cell_S:
 wb.save(out_file)
 print("data_Cell_S has been written to", out_file)
 
-for entry in data_Cell_N:
-    print(entry)
+# for entry in data_Cell_N:
+#     print(entry)
 
 out_file = "out_N_cell.xlsx"
 wb = Workbook()
@@ -123,8 +151,8 @@ for entry in data_Cell_N:
 wb.save(out_file)
 print("Write data to N_cell OK")
 
-for entry in data_cell_Rank:
-    print(entry)
+# for entry in data_cell_Rank:
+#     print(entry)
 
 out_file = "out_Rank.xlsx"
 wb = Workbook()
@@ -140,3 +168,37 @@ for entry in data_cell_Rank:
     ])
 wb.save(out_file)
 print("Write data to Rank OK")
+
+
+for entry in data_Cell_Start:
+    print(entry)
+
+out_file = "out_Cell_Start.xlsx"
+wb = Workbook()
+ws = wb.active
+ws.append(["Time","PCI"])
+for entry in data_Cell_Start:
+    ws.append([
+        entry.get("Time",""),
+        entry.get("PCI","")
+        
+    ])
+wb.save(out_file)
+print("Write data to Start OK")
+
+
+for entry in data_Cell_Fail:
+    print(entry)
+
+out_file = "out_Fail.xlsx"
+wb = Workbook()
+ws = wb.active
+ws.append(["Time","PCI","CellReselFailureCause"])
+for entry in data_Cell_Fail:
+    ws.append([
+        entry.get("Time",""),
+        entry.get("PCI",""),
+        entry.get("CellReselFailureCause","")
+    ])
+wb.save(out_file)
+print("Write data to Fail OK")
